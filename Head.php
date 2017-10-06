3500b4ea6ab0cdc63b41dd61eda46c6355cf5023
Paul Norby <paulnorby@me.com>
2017-10-06 09:54:21 -0500

    switching to php

--- Head.php
+++ Head.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
-<!--  Paul Norby
-      Portfolio
--->
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title>JavaScript Samples</title>
+	<?php
+      echo '<title>Paul Norby - ' . $page_title . '</title>';
+    ?>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/samples.css">
+	<link rel="stylesheet" type="text/css" href="css/mainStyles.css">
 </head>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    //Insert the page header
+    $page_title = 'Home';
+    require_once('head.php');
+    
+    //Show the navigation menu
+    require_once('navbar.php');
+?>
