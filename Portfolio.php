80f3d5d5c081eb5d0835dcf938803bf6203e75d3
Paul Norby <paulnorby@me.com>
2017-10-06 10:03:46 -0500

    adding portfolio php

---
+++ Portfolio.php
@@ -0,0 +1,75 @@
+<?php
+    //Insert the page header
+    $page_title = 'Portfolio';
+    require_once('head.php');
+    
+    //Show the navigation menu
+    require_once('navbar.php');
+?>
+<body>
+    <div class="container" id="screenContainer">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div class="portfolioHeaders">
+	<h1>Agile Methods</h1>
+	<p>Click on a method to view my experience with these agile development methods</p>
+	</div>
+	</div>
+	
+	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+	<a href="scrum.html">
+	<img src="images01/scrumIcon.png" class="methodIcon">
+	</a>
+	</div>
+	
+	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+	<a href="kanban.html">
+	<img src="images01/kanbanIcon.png" class="methodIcon">
+	</a>
+	</div>
+	
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div class="portfolioHeaders">
+	<h1>Language Portfolio</h1>
+	<p>Click on a language to view code samples</p>
+	</div>
+	</div>
+	
+	<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4">
+	<a href="cSharpSamples.html">
+	<img src="images01/cSharpIcon.png" class="languageIcon">
+	</a>
+	</div>
+	
+	<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4">
+	<a href="javaScriptSamples.html">
+	<img src="images01/javaScriptIcon.png" class="languageIcon">
+	</a>
+	</div>
+	
+	<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4">
+	<a href="javaSamples.html">
+	<img src="images01/javaIcon.png" class="languageIcon">
+	</a>
+	</div>
+	
+	<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4">
+	<a href="rubySamples.html">
+	<img src="images01/rubyIcon.png" class="languageIcon">
+	</a>
+	</div>
+	
+	<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4">
+	<a href="sqlSamples.html">
+	<img src="images01/SQLIcon.png" class="languageIcon">
+	</a>
+	</div>
+	
+	<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4">
+	<a href="phpSamples.html">
+	<img src="images01/phpMySQLIcon.png" class="languageIcon">
+	</a>
+	</div>
+	</div>
+
+</body>
+</html>
