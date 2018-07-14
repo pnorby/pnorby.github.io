e5333eddec0b45f03c18e44c3ac0acc85cf417a1
Paul Norby <paulnorby@me.com>
2018-07-14 15:23:55 -0500

    Making updates to education page

---
+++ education.js
@@ -0,0 +1,22 @@
+//put an id on div's with panel primary
+ 
+
+function selectEducation(){
+
+    if (!document.getElementByClassName("panel panel-success"){
+
+        } else {
+
+            var selectedClass = document.getElementByClassName("panel panel-succes");
+
+            selectedClass.className = "panel panel-primary";
+
+        }
+
+               
+
+    var newSelectedClass = document.getElementById(this.value);
+
+    newSelectedClass.className = "panel panel-success";
+
+}
