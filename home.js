96c80104b0cd14a852f9d16844953cd3d32b39d1
pnorby <paulnorby@me.com>
2018-07-23 12:42:33 -0500

    got working, adding 2 seconds to transition

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 function homeLoad(){
     document.getElementById("indexTextBox").style.color = "red";
 	
-    document.getElementById("indexTextBox").style.transition = "opacity 3s";
+    document.getElementById("indexTextBox").style.transition = "opacity 5s";
     document.getElementById("indexTextBox").style.opacity = "1";
 }
