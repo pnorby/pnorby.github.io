611f851f44002b85c77832e6b63c655f2ad5418f
pnorby <paulnorby@me.com>
2018-07-23 12:00:59 -0500

    debugging

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 function homeLoad(){
-    var title = document.getElementById("indexTextBox");
+    //var title = document.getElementById("indexTextBox");
 	console.log("made it in");
-	title.style.transition = "opacity .5";
+	//title.style.transition = "opacity .5";
 }
