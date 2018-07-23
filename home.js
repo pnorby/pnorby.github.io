8aa684c94e5d650545d994325b9689a7483a0e2b
pnorby <paulnorby@me.com>
2018-07-23 12:38:27 -0500

    almost there

--- home.js
+++ home.js
@@ -1,7 +1,6 @@
 function homeLoad(){
     document.getElementById("indexTextBox").style.color = "red";
-	console.log("made it");
 	
     document.getElementById("indexTextBox").style.transition = "opacity 3s";
-    document.getElementById("indexTextBox").style.opacity = ".5";
+    document.getElementById("indexTextBox").style.opacity = "1";
 }
