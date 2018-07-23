89f5c5ab9403283167c5b747b93a21f19dfd4835
pnorby <paulnorby@me.com>
2018-07-23 12:10:34 -0500

    debugging

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 function homeLoad(){
     document.getElementById("indexTextBox").style.color = "red";
-	console.log("made it in");
+	console.log("made it");
 	
 	//title.style.transition = "opacity .5";
 }
