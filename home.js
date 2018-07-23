f479dba34a465b78349c3c13ea80a896b58bc949
pnorby <paulnorby@me.com>
2018-07-23 12:43:51 -0500

    adjusting transition time

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 function homeLoad(){
     document.getElementById("indexTextBox").style.color = "red";
 	
-    document.getElementById("indexTextBox").style.transition = "opacity 5s";
+    document.getElementById("indexTextBox").style.transition = "opacity 8s";
     document.getElementById("indexTextBox").style.opacity = "1";
 }
