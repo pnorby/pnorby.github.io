5dc2d4f4adf4020a3d836645636cfc9fea5122b4
pnorby <paulnorby@me.com>
2018-07-23 12:35:07 -0500

    trying with transition instead of webkittransition

--- home.js
+++ home.js
@@ -1,6 +1,7 @@
 function homeLoad(){
     document.getElementById("indexTextBox").style.color = "red";
-	console.log("made it in");
+	console.log("made it");
 	
-    document.getElementById("indexTextBox").style.webkitTransition = "opacity .7 3s";
+    document.getElementById("indexTextBox").style.transition = "opacity 3s";
+    document.getElementById("indexTextBox").style.opacity = ".5";
 }
