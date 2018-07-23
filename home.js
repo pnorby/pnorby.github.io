a0e7804fdaf6f43a146ebf3f1b0507536db32599
pnorby <paulnorby@me.com>
2018-07-23 12:24:32 -0500

    added the opacity setting

--- home.js
+++ home.js
@@ -2,5 +2,5 @@ function homeLoad(){
     document.getElementById("indexTextBox").style.color = "red";
 	console.log("made it in");
 	
-    document.getElementById("indexTextBox").style.webkitTransition = "opacity 3s";
+    document.getElementById("indexTextBox").style.webkitTransition = "opacity .7 3s";
 }
