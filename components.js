901308613f215caebe7dfea0b52ccfd30ec50ac4
Paul Norby <Paul.Norby@etcconnect.com>
2021-11-18 10:02:53 -0600

    Some cleanup and refactoring of bootstrap 4 implementation

--- components.js
+++ components.js
@@ -6,7 +6,6 @@ class MyNavBar extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Navbar w/ text</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -42,9 +41,6 @@ class MyNavBar extends HTMLElement {
         </div>
       </li>
         </ul>
-        <span class="navbar-text">
-          Navbar text with an inline element
-        </span>
       </div>
     </nav>
       `;
