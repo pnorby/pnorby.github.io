5123b1c62586eb79c89dad25bf4a433837f8bcec
Paul Norby <Paul.Norby@etcconnect.com>
2021-11-22 15:05:02 -0600

    Starting an overhaul

--- components.js
+++ components.js
@@ -11,10 +11,10 @@ class MyNavBar extends HTMLElement {
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
+          <!--<li class="nav-item">
             <a class="nav-link" href="AboutTheDeveloper.html">About</a>
-          </li>
-          <li class="nav-item dropdown">
+          </li>-->
+          <!--<li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Education
         </a>
@@ -22,10 +22,10 @@ class MyNavBar extends HTMLElement {
           <a class="dropdown-item" href="education.html">Madison College</a>
           <a class="dropdown-item" href="certificates.html">Certifications</a>         
         </div>
-      </li>
-          <li class="nav-item">
+      </li>-->
+          <!--<li class="nav-item">
             <a class="nav-link" href="Experience.html">Experience</a>
-          </li>
+          </li>-->
           <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Portfolio
@@ -37,9 +37,12 @@ class MyNavBar extends HTMLElement {
         <a class="dropdown-item" href="angularSamples.html">Angular</a>  
         </div>
       </li>
-      <!--<li class="nav-item">
+      <li class="nav-item">
+            <a class="nav-link" href="Experience.html">References</a>
+          </li>
+      <li class="nav-item">
             <a class="nav-link" href="Suggestions.html">Feedback</a>
-          </li>-->
+          </li>
         </ul>
       </div>
     </nav>
