f77eb854bfc8475a5c80c467ab48588176c24bd9
Paul Norby <Paul.Norby@etcconnect.com>
2021-11-29 08:58:04 -0600

    Begin revamp of experience

--- components.js
+++ components.js
@@ -23,9 +23,9 @@ class MyNavBar extends HTMLElement {
           <a class="dropdown-item" href="certificates.html">Certifications</a>         
         </div>
       </li>-->
-          <!--<li class="nav-item">
+          <li class="nav-item">
             <a class="nav-link" href="Experience.html">Experience</a>
-          </li>-->
+          </li>
           <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Portfolio
